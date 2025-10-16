3accf23940816ec203648445809d9f47f5d9c114
Huy-NTG <giahuy120304@gmail.com>
2025-10-16 23:05:28 +0700

    create structure frontend and add adminPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import AdminPage from './pages/Adminpage/AdminPage';
+import DefaultPage from './pages/DefaultPage/DefaultPage';
+import AdminDashboard from './pages/Adminpage/AdminDashboard/AdminDashboard';
+import AdminBookings from './pages/Adminpage/AdminBookings/AdminBookings';
+import AdminFlights from './pages/Adminpage/AdminFlights/AdminFlights';
+import AdminHotels from './pages/Adminpage/AdminHotels/AdminHotels';
+import AdminTours from './pages/Adminpage/AdminTours/AdminTours';
+import AdminUsers from './pages/Adminpage/AdminUsers/AdminUsers';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        {/* Trang mặc định */}
+        <Route path="/" element={<Navigate to="/default" />} />
+        <Route path="/default" element={<DefaultPage />} />
+
+        {/* Trang quản trị - có sidebar cố định */}
+        <Route path="/admin" element={<AdminPage />}>
+          {/* Đây là các route con nằm trong layout AdminPage */}
+          <Route index element={<AdminDashboard />} /> {/* Khi vào /admin thì load Dashboard */}
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="tours" element={<AdminTours />} />
+          <Route path="flights" element={<AdminFlights />} />
+          <Route path="hotels" element={<AdminHotels />} />
+          <Route path="users" element={<AdminUsers />} />
+          <Route path="bookings" element={<AdminBookings />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
